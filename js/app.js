4d80d2f60875af63f10d1c53573cd124ab246c2a
Maciek Blim <m.blim@binarapps.com>
2016-01-06 22:00:29 +0100

    making HTTP request from inside main fn

--- js/app.js
+++ js/app.js
@@ -3,21 +3,28 @@ import Cycle from '@cycle/core';
 /*eslint-disable no-unused-vars*/
 import {makeDOMDriver, hJSX} from '@cycle/dom';
 /*eslint-enable no-unused-vars*/
+import {makeHTTPDriver} from '@cycle/http';
 
-function main({DOM}) {
+function main({DOM, HTTP}) {
     const submitForm$ = DOM.select('#repo')
         .events('submit')
         .do(ev => ev.preventDefault());
 
-    const repo$ = DOM.select('.repo-name')
+    const input$ = DOM.select('.repo-name')
         .events('input')
         .map(ev => ev.target.value);
 
-    const changeRepo$ = submitForm$.withLatestFrom(repo$, (sf, repo) => repo);
+    const changeRepo$ = submitForm$.withLatestFrom(input$, (sf$, name) => name);
 
-    const vtree$ = changeRepo$
-        .startWith('')
-        .map((repo) =>
+    const GITHUB_SEARCH_API = 'https://api.github.com/repos/';
+
+    const request$ =  changeRepo$.map(name => `${GITHUB_SEARCH_API}${name}/contents`);
+
+    const vtree$ = HTTP
+        .flatMap(x => x)
+        .map(res => res.body)
+        .startWith([])
+        .map(files =>
             <div>
                 <form id="repo" className="form-inline">
                     <div className="form-group">
@@ -27,16 +34,22 @@ function main({DOM}) {
                     <input type="submit" className="btn btn-default" value="Get repo filesystem!" />
                 </form>
                 <hr />
-                {repo}
+                <div className="row">
+                    <div className="col-md-12">
+                        {JSON.stringify(files)}
+                    </div>
+                </div>
             </div>
         );
     return {
-        DOM: vtree$
+        DOM: vtree$,
+        HTTP: request$
     };
 }
 
 const drivers = {
-     DOM: makeDOMDriver('#app')
+     DOM: makeDOMDriver('#app'),
+     HTTP: makeHTTPDriver()
 };
 
 Cycle.run(main, drivers);
