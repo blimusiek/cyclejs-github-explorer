7d14fecf1a3e19402c809cbf51e0239d76ed771d
Maciek Blim <m.blim@binarapps.com>
2016-01-06 19:13:11 +0100

    basic repo name form (with submit event)

--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
 /** @jsx hJSX */
 import Cycle from '@cycle/core';
-import {Observable} from 'rx';
 /*eslint-disable no-unused-vars*/
 import {makeDOMDriver, hJSX} from '@cycle/dom';
 /*eslint-enable no-unused-vars*/
 
-function main() {
+function main({DOM}) {
+    const submitForm$ = DOM.select('#repo')
+        .events('submit')
+        .do(ev => ev.preventDefault());
+
+    const repo$ = DOM.select('.repo-name')
+        .events('input')
+        .map(ev => ev.target.value);
+
+    const changeRepo$ = submitForm$.withLatestFrom(repo$, (sf, repo) => repo);
+
+    const vtree$ = changeRepo$
+        .startWith('')
+        .map((repo) =>
+            <div>
+                <form id="repo" className="form-inline">
+                    <div className="form-group">
+                        <label for="repo-name">Full Repo Name</label>
+                        <input type="text" name="repo-name" className="form-control repo-name" />
+                    </div>
+                    <input type="submit" className="btn btn-default" value="Get repo filesystem!" />
+                </form>
+                <hr />
+                {repo}
+            </div>
+        );
     return {
-        DOM: Observable.timer(0, 1000)
-            .map(i => <div>Seconds elapsed {i}</div>)
+        DOM: vtree$
     };
 }
 
