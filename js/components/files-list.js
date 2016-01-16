3f5b1fbc1de5ff436ecb18286f4dde0c29b0f2b0
Maciek Blim <m.blim@binarapps.com>
2016-01-16 13:05:37 +0100

    refactored to use navigation component

--- js/components/files-list.js
+++ js/components/files-list.js
@@ -2,36 +2,22 @@
 /*eslint-disable no-unused-vars*/
 import {hJSX} from '@cycle/dom';
 /*eslint-enable no-unused-vars*/
+import Navigation from './navigation';
 
 export default function FilesList ({DOM, HTTP, props$}) {
     const GITHUB_SEARCH_API = 'https://api.github.com/repos/';
-    const ROOT_REPO_PATH = '/';
-    const PATH_SEPARATOR = '/';
-    const BACK_SIGNAL = '..';
-
-    const backClick$ = DOM.select('.go-back')
-        .events('click')
-        .do(ev => ev.preventDefault())
 
     const dirClick$ = DOM.select('.enter-dir')
         .events('click')
         .do(ev => ev.preventDefault())
         .map(ev => ev.target.textContent);
 
-    const path$ = dirClick$
-        .merge(props$.map(() => null))
-        .merge(backClick$.map(() => BACK_SIGNAL))
-        .scan((p, c) => {
-            // we need to reset path when someone changed repo
-            if (null === c) return ROOT_REPO_PATH;
-            if (BACK_SIGNAL === c) return p.split(PATH_SEPARATOR).slice(0, -1).join(PATH_SEPARATOR);
-            return `${p}${PATH_SEPARATOR}${c}`;
-        }, '')
-        .map(path => path.replace('//', ROOT_REPO_PATH))
-        .do(x => console.log(x))
-        .startWith(ROOT_REPO_PATH);
+    const navigation = Navigation({
+        DOM: DOM,
+        props: { dirClick$: dirClick$.merge(props$.map(() => null)) }
+    });
 
-    const request$ = path$
+    const request$ = navigation.path$
         .withLatestFrom(props$.map(name => `${GITHUB_SEARCH_API}${name}/contents`), (path, url) => `${url}${path}`);
 
     const vtree$ = HTTP
@@ -42,21 +28,13 @@ export default function FilesList ({DOM, HTTP, props$}) {
             if (a.type < b.type) return -1;
             return 0;
         }))
-        .withLatestFrom(path$, (files, path) => { return {files: files, path: path} })
-        .startWith({path: ROOT_REPO_PATH, files: []})
+        .startWith([])
+        .withLatestFrom(navigation.DOM, (files, Path) => { return {files, Path} })
         .map(dir =>
             <div className="row">
                 <div className="col-md-12">
                     <table className="table">
-                        <caption>{dir.path}</caption>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th className="text-right">
-                                    {ROOT_REPO_PATH !== dir.path ? <button className="btn btn-default btn-xs go-back">Go Back</button> : ''}
-                                </th>
-                            </tr>
-                        </thead>
+                        {dir.Path}
                         <tbody>
                             {dir.files
                                 .map(file =>
---
+++ js/components/navigation.js
@@ -0,0 +1,45 @@
+/** @jsx hJSX */
+/*eslint-disable no-unused-vars*/
+import {hJSX} from '@cycle/dom';
+/*eslint-enable no-unused-vars*/
+
+
+export default function FilesList ({DOM, props}) {
+    const ROOT_REPO_PATH = '/';
+    const PATH_SEPARATOR = '/';
+    const BACK_SIGNAL = '..';
+
+    const backClick$ = DOM.select('.go-back')
+        .events('click')
+        .do(ev => ev.preventDefault());
+
+    const path$ = props.dirClick$
+        .merge(backClick$.map(() => BACK_SIGNAL))
+        .scan((p, c) => {
+            // we need to reset path when someone changed repo
+            if (null === c) return ROOT_REPO_PATH;
+            if (BACK_SIGNAL === c) return p.split(PATH_SEPARATOR).slice(0, -1).join(PATH_SEPARATOR);
+            return `${p}${PATH_SEPARATOR}${c}`;
+        }, '')
+        .map(path => path.replace('//', ROOT_REPO_PATH))
+        .startWith(ROOT_REPO_PATH);
+
+    const vtree$ = path$.map(path =>
+        <span>
+            <caption>{path}</caption>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th className="text-right">
+                        {ROOT_REPO_PATH !== path ? <button className="btn btn-default btn-xs go-back">Go Back</button> : ''}
+                    </th>
+                </tr>
+            </thead>
+        </span>
+    )
+
+    return {
+        DOM: vtree$,
+        path$
+    };
+}
